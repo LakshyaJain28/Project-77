0da6c8b002724a8d76171a2c4a07bffbef83e8ba
LakshyaJain28 <79844523+LakshyaJain28@users.noreply.github.com>
2021-06-01 16:05:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var line="my first console project";
+console.log(line);
+var fname="Lakshya";
+var lname="Jain";
+console.log(fname.concat(lname));
+var a=10;
+var b=5;
+var add=a+b;
+console.log(add);
